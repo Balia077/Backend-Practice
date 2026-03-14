8a06cc1bee5b26120c043b3dc97c4e10e8102644
Balia077 <balaramd894@gmail.com>
2026-03-14 11:39:32 +0530

    Implement product routes and server status response

--- Level-1-Core-Basics/Task-3/src/app.js
+++ Level-1-Core-Basics/Task-3/src/app.js
@@ -2,5 +2,41 @@ const express = require('express');
 
 
 const app = express();
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send("Server is up and running");
+})
+
+const products = [];
+
+app.get('/products', (req, res) => {
+    res.status(200).json({
+        message: "All Products",
+        data: products
+    })
+})
+
+app.post('/products', (req, res) => {
+    const newProduct = {
+        product: req.body.product,
+        price: req.body.price
+    }
+    products.push(newProduct);
+    res.status(201).json({
+        message: "Product Created",
+        data: newProduct
+    })
+})
+
+app.delete('/products/:id', (req, res) => {
+    const index = req.params.id;
+    console.log(index)
+    products.splice(index, 1);
+    res.status(200).json({
+        message: `Product with id ${req.params.id} deleted`
+    })
+})
 
 module.exports = app;
